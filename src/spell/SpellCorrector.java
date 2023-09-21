7c27012156e688b76e0eb9d37209db3898875d3f
Tommy Valletta <97928519+Tommy-Valletta@users.noreply.github.com>
2023-09-20 18:04:06 -0600

    Pushing the project in its current form (Sept 2023)

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -1,10 +1,30 @@
 package spell;
-
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class SpellCorrector implements ISpellCorrector {
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
+
+        // pass the filename to the file as a parameter
+        File file = new File(dictionaryFileName);
+        Scanner sc = new Scanner(file);
+
+        List<String> rawStrings = new ArrayList<String>();
+        while (sc.hasNextLine())
+            rawStrings.add(sc.nextLine());
+        //System.out.println(rawStrings);
+
+        ITrie myTrie = new Trie();
+        int i = 0;
+        while (i < rawStrings.size()) {
+            //System.out.println(rawStrings.get(i));
+            myTrie.add(rawStrings.get(i));
+            i++;
+        }
 
     }
 
--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -1,9 +1,58 @@
 package spell;
 
 public class Trie implements ITrie {
+    private int wordCount = 0;
+    private int nodeCount = 1;
+    private INode rootNode = new Node();
+
+    @Override
+    public String toString() {
+        StringBuilder word = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+
+        toStringHelper(rootNode, word, output);
+
+        return output.toString();
+    }
+
+    private void toStringHelper(INode node, StringBuilder word, StringBuilder output) {
+        if(node.getValue() > 0) {
+            output.append(word.toString());
+            output.append("\n");
+        }
+
+        INode [] children = node.getChildren();
+
+        for(int i = 0; i < children.length; i++) {
+            if(children[i] != null) {
+                char childLetter = (char)('a' + i);
+                word.append(childLetter);
+
+                toStringHelper(children[i], word, output);
+
+                word.deleteCharAt(word.length() - 1);
+            }
+        }
+    }
+
+
     @Override
     public void add(String word) {
+        INode localNode = rootNode;
 
+        for(int i = 0; i < word.length(); i++){
+            char currChar = word.charAt(i);
+            int charIndex = currChar - 'a';
+            INode[] children = localNode.getChildren();
+            if (children[charIndex] == null) {
+                children[charIndex] = new Node();
+                nodeCount++;
+            }
+            localNode = children[charIndex];
+        }
+
+        localNode.incrementValue();
+        wordCount++;
     }
 
     @Override
@@ -13,11 +62,23 @@ public class Trie implements ITrie {
 
     @Override
     public int getWordCount() {
-        return 0;
+        return wordCount;
     }
 
     @Override
     public int getNodeCount() {
-        return 0;
+        return nodeCount;
+    }
+    @Override
+    public boolean equals(Object o) {
+        //if null return false
+        //if not a trie return false
+        //if wordcount not equal return false
+        //if nodecount not equal return false
+        return false;//equalsHelper(this.root, other.root);
+    }
+
+    private boolean equalsHelper(INode a, INode b){
+        return false;//Compare a and b
     }
 }
